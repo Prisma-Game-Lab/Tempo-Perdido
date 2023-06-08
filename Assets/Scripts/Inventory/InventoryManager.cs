48fbbb74ca1a5cfb9bc24c162fc58f41385354f2
AlOzorio <arthurlozorio@gmail.com>
2023-06-08 16:00:19 -0300

    Update inventory scriptable object

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -6,12 +6,19 @@ using UnityEngine.UI;
 public class InventoryManager : MonoBehaviour
 {
     [SerializeField] private InventorySO inventory;
-    List<Sprite> inventoryItems = new List<Sprite>();
-    int selectedItem = 0;
+    public List<GameObject> inventoryItems = new List<GameObject>();
+    public GameObject selector;
+    private int selectedItem = 0;
+
+    void Start()
+    {
+        UpdateView();
+    }
 
     public void ChangeSelectedItem(int index)
     {
         selectedItem = index;
+        selector.transform.position = inventoryItems[index].transform.position;
     }
 
     public void AddItem(Collectable collectable)
@@ -29,6 +36,19 @@ public class InventoryManager : MonoBehaviour
 
     public void UpdateView()
     {
+        for (int i = 0; i < inventoryItems.Count; i++)
+        {
+            Image renderer = inventoryItems[i].GetComponent<Image>();
 
+            if (inventory.inventoryItems[i].sprite == null)
+            {
+                renderer.enabled = false;
+            }
+            else
+            {
+                renderer.enabled = true;
+                renderer.sprite = inventory.inventoryItems[i].sprite;
+            }
+        }
     }
 }
