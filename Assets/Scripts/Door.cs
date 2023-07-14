e6cc2ed58122f33ddf9f5a421fb6f346be5007fb
AlOzorio <arthurlozorio@gmail.com>
2023-07-14 18:40:23 -0300

    Friend's house scene transition

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : ClickManager
+{
+    [SerializeField] private DialogueSO monologue;
+    [SerializeField] private InventorySO inventory;
+    public int sceneIndex;
+    public string requiredKey;
+    public DialogueManager dialogueManager;
+
+
+    public override IEnumerator MoveToPoint(Vector2 point)
+    {
+        yield return base.MoveToPoint(point);
+        movementSO.initialPosition = Player.position;
+        if (!interrupted)
+        {
+            ChangeScene();
+        }
+        interrupted = false;
+    }
+
+    private void ChangeScene()
+    {
+        if (inventory.inventoryItems.Find(x => x.key == requiredKey) != null)
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+        else
+        {
+            dialogueManager.EnqueueDialogue(monologue.dialogue);
+            dialogueManager.StartDialogue(monologue, false);
+        }
+    }
+}
